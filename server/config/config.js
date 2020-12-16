18e1bd449fd1a4d35fcc10ea65e5869da821ba81
RaymundoSantorski <rayma9829@gmail.com>
2020-12-15 22:13:42 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -4,11 +4,11 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://cafe-user:Magr980929@cluster0.yduod.mongodb.net/cafe?retryWrites=true&w=majority';
-// }
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URL;
+}
 
 process.env.URLDB = urlDB;
