f279f10d191abb3c6ee65b4b714957ede101b05d
RaymundoSantorski <rayma9829@gmail.com>
2020-12-27 13:34:25 -0600

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-/* Verificar token */
+// Verificar token 
 let verificaToken = (req, res, next) => {
     let token = req.get('token');
     jwt.verify(token, process.env.SEED, (err, decoded) => {
@@ -17,7 +17,7 @@ let verificaToken = (req, res, next) => {
     });
 };
 
-/* Verifica admin_role */
+// Verifica admin_role
 let verificaAdmin_Role = (req, res, next) => {
     if (req.usuario.role === 'ADMIN_ROLE') {
         next();
@@ -31,7 +31,27 @@ let verificaAdmin_Role = (req, res, next) => {
     });
 };
 
+// Verifica Token Img
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+
+}
+
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 };
